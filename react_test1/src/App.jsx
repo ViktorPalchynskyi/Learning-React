a0ad1f9ca4a1e05b1942782b9ce7e8c4d5c06afa
Viktor <palchinskij19@gmail.com>
2024-10-08 19:38:27 +0300

    08.10.24 - useLayoutEffect

--- react_test1/src/App.jsx
+++ react_test1/src/App.jsx
@@ -1,6 +1,16 @@
 import { Article } from './components/Article/Article';
 
 function App() {
+    useLayoutEffect(() => {
+        const box = document.getElementById('box');
+        const { height } = box.getBoundingClientRect();
+
+        if (height > 300) {
+            // Программно обновляем элемент на основе его высоты
+            box.style.backgroundColor = 'red';
+        }
+    }, []); // Эффект выполняется синхронно перед обновлением экрана
+
     return (
         <>
             <h1>Hello there</h1>
